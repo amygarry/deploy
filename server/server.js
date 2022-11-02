e6aed32dae8055d1683a06d9bf2d8f2170157442
Amy Garry <amyegarry@gmail.com>
2022-11-02 14:40:42 -0600

    copied

---
+++ server/server.js
@@ -0,0 +1,16 @@
+const express  = require("express")
+require("dotenv").config()
+
+
+const app = express()
+
+app.use(express.json())
+
+
+const{home}=require("./controllers/pageCtrl")
+
+app.get("/", home)
+
+const {PORT} = process.env
+
+app.listen(PORT, ()=> console.log(`Server listening on port ${PORT}`))
